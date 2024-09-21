f543304aa592454e7af088efa05591d04d212238
YATISH NAGARAJ <yatishnagaraj@Simhasenappa.lan>
2024-09-21 22:18:16 +0530

    corrected workspace name

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "google_compute_firewall" "fw-rule" {
 
   allow {
     protocol = "${var.protocol}"
-    ports    = ["${var.ports}"]
+    ports    = "${var.ports}"
   }
-  target_tags   = ["${var.network}-${var.name}"]
-  source_ranges = ["${var.source_ranges}"]
+  target_tags   = "${var.network}-${var.name}"
+  source_ranges = "${var.source_ranges}"
 }
